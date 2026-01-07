3ac25877742a53dc915fe9a1fad03cd2c1e85b85
Hlib <horbachovhlib@gmail.com>
2026-01-07 18:53:24 +0100

    final

--- ft_printf/src/ft_print_nbr.c
+++ ft_printf/src/ft_print_nbr.c
@@ -6,7 +6,7 @@
 /*   By: hlib <hlib@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 20:39:27 by hlib              #+#    #+#             */
-/*   Updated: 2026/01/07 15:56:54 by hlib             ###   ########.fr       */
+/*   Updated: 2026/01/07 18:48:16 by hlib             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ int	ft_print_nbr(int nbr)
 {
 	char	*s;
 	int		len;
+	int		i;
 
 	s = ft_itoa(nbr);
 	if (!s)
 		return (0);
 	len = ft_strlen(s);
-	while (*s)
+	i = 0;
+	while (s[i])
 	{
-		ft_print_char(*s);
-		s++;
+		ft_print_char(s[i]);
+		i++;
 	}
+	free(s);
 	return (len);
 }
